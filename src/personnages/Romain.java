d6a2565621a3b8b9e72f0676d8a245f33e059c66
NoNo3103 <114724169+NoNo3103@users.noreply.github.com>
2022-10-06 19:55:48 +0200

    Update Romain.java

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,34 +1,24 @@
 public class Romain {
 	private String nom;
 	private int force;
-	private Equipement equipements[] = new Equipement[2];
-	private int nbEquipement = 0;
-	
 	public Romain(String nom, int force) {
-		assert force >= 0;
 		this.nom = nom;
 		this.force = force;
 	}
 	public String getNom() {
 		return nom;
 	}
-	public int getForce() {
-		return force;
-	}
 	public void parler(String texte) {
-		System.out.println(prendreParole() + "'" + texte + "'");
+		System.out.println(prendreParole() + "« " + texte + "»");
 	}
 	private String prendreParole() {
 		return "Le romain " + nom + " : ";
 	}
 	public void recevoirCoup(int forceCoup) {
-		assert force >= 0;
-		int t = force;
 		force -= forceCoup;
-		if (force > 0) {
-			parler("Aie");
-		} else {
-			parler("J'abandonne...");
+	if (force > 0) {
+		parler("Aïe");
+	} else {
+		parler("J'abandonne...");
 	}
-		assert t-force > 0;
 	}
