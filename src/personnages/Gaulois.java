c3d5bee2bd23faa545e5ac9682ea34952182d862
paris noah <prn4448a@ens.fsi>
2022-10-07 09:22:49 +0200

    LesGaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,5 +1,38 @@
 package personnages;
 
 public class Gaulois {
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+	private String nom;
+	private int force;
+	private int effetPotion =1;
+	
+	public String getNom() {
+		return nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}	
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<<" + texte + ">>");
+		
+	}
+	private String prendreParole() {
+		return "Le gaulois" + nom + ":" ;
+		// TODO Auto-generated method stub
+	public void frapper(Romain romain) {
+		System.out.println(nom + "envoie un grand coup dans la machoire" + romain.getNom());
+		
+	}
+	}
+
+	public static void main(String[] args) {
+		
+	}
 
 }
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -2,4 +2,9 @@ package personnages;
 
 public class Romain {
 
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
